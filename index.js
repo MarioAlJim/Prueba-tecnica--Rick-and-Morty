88167c6e10770fef6d1ca5f2d0c6d308fda7a5d2
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-13 14:16:43 -0600

    feat: add api query
    Added base API query to consult characteres

---
+++ index.js
@@ -0,0 +1,15 @@
+async function getApiData() {
+    try {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }   
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+getApiData();
