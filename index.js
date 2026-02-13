92db91ef070b5e440a39b9474299b74a5a0bebcf
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-13 15:42:19 -0600

    refactor: added main method
    added main method to filter info and print

--- index.js
+++ index.js
@@ -5,11 +5,36 @@ async function getApiData() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data);
-    }
-    catch (error) {
+        if (!data) {
+            throw new Error('Data format error: results property is missing');
+        }
+        return data.results;
+    } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-getApiData();
+async function filterCharactersByStatus(status) {
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?status=${status}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function main() {
+    try {
+        const characters = await getApiData();
+        const aliveCharacters = characters.filter(character => character.status === 'Alive');
+        aliveCharacters.forEach(character => {
+            console.log(`id: ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
+        });
+    } catch (error) {
+        console.error('Error in main function:', error);
+    }
+}
