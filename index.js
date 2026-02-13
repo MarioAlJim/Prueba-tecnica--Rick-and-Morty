6ad2e11c8119894fa821c438e54237181d070e40
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-13 17:37:32 -0600

    refactor: simplified methods
    simplified methods and separation of responsibilities
    Tests updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Fetches character data from the Rick and Morty API and filters characters based on their status.
  */
-async function getApiData() {
+export async function getApiData() {
     try {
         const response = await fetch('https://rickandmortyapi.com/api/character');
         if (!response.ok) {
@@ -17,41 +17,41 @@ async function getApiData() {
 /**
  * Filters characters based on their status and returns an array of objects containing the id, name, and status of each character.
  */
-export async function filterCharacters(status) {
-    try {
-        const characters = await getApiData();
-        let filteredCharacters = characters.filter(character => character.status === status);
-        filteredCharacters = filteredCharacters.map(character => ({ id: character.id, name: character.name, status: character.status }));
-        return filteredCharacters;
-    } catch (error) {
-        console.error('Error in main function:', error);
-    }
+export function filterByStatus(characters, status) {
+    let filteredCharacters = characters.filter(character => character.status === status);
+    filteredCharacters = filteredCharacters.map(character => 
+        ({ id: character.id, name: character.name, status: character.status })
+    );
+    return filteredCharacters;
 }
 
 /**
  * Replaces spaces with underscores in the names of the characters and 
  * returns an array of objects containing the id, name, and status of each character.
  */
-export async function replaceSpaces(characters) {
-    try {
-        const updatedCharacters = characters.map(character => ({
-                ...character,
-                name: character.name.replace(/ /g, '_')
-            }));
-        return updatedCharacters;
-    } catch (error) {
-        console.error('Error in replaceSpaces function:', error);
-    }
+export function spaceInNameReplacer(characters) {
+    const updatedCharacters = characters.map(character => ({
+            ...character,
+            name: character.name.replace(/ /g, '_')
+        }));
+    return updatedCharacters;
 }
 
 /**
  * Main function to execute the character filtering and name replacement, and log the results to the console.
  */
 async function main() {
-    const characters = await filterCharacters('Alive');
-    const updateNames= await replaceSpaces(characters);
-    const result  = JSON.stringify(updateNames, null, 2);
+    const result = [];
+    try {
+        const baseData = await getApiData();
+        const characters = filterByStatus(baseData, 'Alive');
+        const updateNames = spaceInNameReplacer(characters);
+        result.push(...updateNames);
+    } catch (error) {
+        console.error('Error in main function:', error);
+    }
     console.log(result);
+    return JSON.stringify(result);
 }
 
 main();
--- mainTest.js
+++ mainTest.js
@@ -1,41 +1,35 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import { filterCharacters, replaceSpaces } from './index.js';
+import { getApiData, filterByStatus, spaceInNameReplacer } from './index.js';
 
 test ('main function should log alive characters only for page 1', async () => {
-    const result = await filterCharacters('Alive');
+    const baseData = await getApiData();
+    const result = filterByStatus(baseData, 'Alive');
     assert(result.length > 0, 'No characters were returned');
     const aliveCharacters = result.filter(character => character.status === 'Alive');
     assert(aliveCharacters.length === 8, 'No alive characters were returned');
-    // aliveCharacters.forEach(character => {
-    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
-    // });
 });
 
 test ('main function should log dead characters only for page 1', async () => {
-    const result = await filterCharacters('Dead');
+    const baseData = await getApiData();
+    const result = filterByStatus(baseData, 'Dead');
     assert(result.length > 0, 'No characters were returned');
     const deadCharacters = result.filter(character => character.status === 'Dead');
     assert(deadCharacters.length === 6, 'No dead characters were returned');
-    // deadCharacters.forEach(character => {
-    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
-    // });
 });
 
 test ('main function should log unknown characters only for page 1', async () => {
-    const result = await filterCharacters('unknown');
+    const baseData = await getApiData();
+    const result = filterByStatus(baseData, 'unknown');
     assert(result.length > 0, 'No characters were returned');
     const unknownCharacters = result.filter(character => character.status === 'unknown');
     assert(unknownCharacters.length === 6, 'No unknown characters were returned');
-    // unknownCharacters.forEach(character => {
-    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
-    // });
 });
 
-
 test('should replace spaces with underscores in character names, alive', async () => {
-    const characters = await filterCharacters('Alive');
-    const result = await replaceSpaces(characters);
+    const baseData = await getApiData();
+    const characters = filterByStatus(baseData, 'Alive');
+    const result = spaceInNameReplacer(characters);
     result.forEach((character, index) => {
         const expectedName = characters[index].name.replace(/ /g, '_');
         assert.strictEqual(character.name, expectedName);
@@ -43,8 +37,9 @@ test('should replace spaces with underscores in character names, alive', async (
 });
 
 test('should replace spaces with underscores in character names, dead', async () => {
-    const characters = await filterCharacters('Dead');
-    const result = await replaceSpaces(characters);
+    const baseData = await getApiData();
+    const characters = filterByStatus(baseData, 'Dead');
+    const result = spaceInNameReplacer(characters);
     result.forEach((character, index) => {
         const expectedName = characters[index].name.replace(/ /g, '_');
         assert.strictEqual(character.name, expectedName);
