5695f639e2d4ca5f32c54581c969ad189808f325
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-13 16:43:21 -0600

    feat: add space replacer
    added function for replace spaces

--- index.js
+++ index.js
@@ -5,36 +5,44 @@ async function getApiData() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        if (!data) {
-            throw new Error('Data format error: results property is missing');
-        }
         return data.results;
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-async function filterCharactersByStatus(status) {
+// async function filterCharactersByStatus(status) {
+//     try {
+//         const response = await fetch(`https://rickandmortyapi.com/api/character/?status=${status}`);
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//     }
+// }
+
+export async function filterCharactersByStatus(status) {
     try {
-        const response = await fetch(`https://rickandmortyapi.com/api/character/?status=${status}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log(data);
+        const characters = await getApiData();
+        let filteredCharacters = characters.filter(character => character.status === status);
+        filteredCharacters = filteredCharacters.map(character => ({ id: character.id, name: character.name, status: character.status }));
+        return filteredCharacters;
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error in main function:', error);
     }
 }
 
-async function main() {
+export async function replaceSpaces(characters) {
     try {
-        const characters = await getApiData();
-        const aliveCharacters = characters.filter(character => character.status === 'Alive');
-        aliveCharacters.forEach(character => {
-            console.log(`id: ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
-        });
+        const updatedCharacters = characters.map(character => ({
+                ...character,
+                name: character.name.replace(/ /g, '_')
+            }));
+        return updatedCharacters;
     } catch (error) {
-        console.error('Error in main function:', error);
+        console.error('Error in replaceSpaces function:', error);
     }
 }
