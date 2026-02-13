fbf317495159d40d45e70c7d638b09f1a9f99260
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2026-02-13 16:46:30 -0600

    test: add test for main functions
    added test for filter function and replacer function

---
+++ mainTest.js
@@ -0,0 +1,55 @@
+import test from 'node:test';
+import assert from 'node:assert';
+import { filterCharactersByStatus, replaceSpaces } from './index.js';
+
+test ('main function should log alive characters only for page 1', async () => {
+    const result = await filterCharactersByStatus('Alive');
+    assert(result.length > 0, 'No characters were returned');
+    const aliveCharacters = result.filter(character => character.status === 'Alive');
+    assert(aliveCharacters.length === 8, 'No alive characters were returned');
+    // aliveCharacters.forEach(character => {
+    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
+    // });
+});
+
+test ('main function should log dead characters only for page 1', async () => {
+    const result = await filterCharactersByStatus('Dead');
+    assert(result.length > 0, 'No characters were returned');
+    const deadCharacters = result.filter(character => character.status === 'Dead');
+    assert(deadCharacters.length === 6, 'No dead characters were returned');
+    // deadCharacters.forEach(character => {
+    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
+    // });
+});
+
+test ('main function should log unknown characters only for page 1', async () => {
+    const result = await filterCharactersByStatus('unknown');
+    assert(result.length > 0, 'No characters were returned');
+    const unknownCharacters = result.filter(character => character.status === 'unknown');
+    assert(unknownCharacters.length === 6, 'No unknown characters were returned');
+    // unknownCharacters.forEach(character => {
+    //   console.log(`"id": ${character.id}, Name: ${character.name}, Status: ${character.status}, gender: ${character.gender}`);
+    // });
+});
+
+
+test('should replace spaces with underscores in character names, alive', async () => {
+    const characters = await filterCharactersByStatus('Alive');
+    const result = await replaceSpaces(characters);
+    result.forEach((character, index) => {
+        const expectedName = characters[index].name.replace(/ /g, '_');
+        assert.strictEqual(character.name, expectedName);
+    });
+});
+
+test('should replace spaces with underscores in character names, dead', async () => {
+    const characters = await filterCharactersByStatus('Dead');
+    const result = await replaceSpaces(characters);
+    result.forEach((character, index) => {
+        const expectedName = characters[index].name.replace(/ /g, '_');
+        assert.strictEqual(character.name, expectedName);
+    });
+});
+
+
+
